470bf999c16f2310b5167cfe1719ae7c82d3be5c
Buddhi Adhikari <buddhiadikari@live.com>
2023-05-06 10:14:39 +0530

    Adding gin package. gePages rqeuest

--- master.go
+++ master.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type Page struct {
+	Id         int64  `json:"id"`
+	Title      string `json:"title"`
+	Body       string `json:"body"`
+	Dedication string `json:"dedication"`
+}
+
+var pages = []Page{
+	{Id: 1, Title: "Page 1", Body: "This is page 1.", Dedication: "To my wife"},
+	{Id: 2, Title: "Page 2", Body: "This is page 2.", Dedication: "To my kids"},
+	{Id: 3, Title: "Page 3", Body: "This is page 3.", Dedication: "To my dog"},
+}
+
+func getPages(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, pages)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	router := gin.Default()
+	router.GET("/pages", getPages)
+	router.Run("localhost:8080")
 }
